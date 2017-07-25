b31cb7f7d613ab37f1721a33193b4270b6401e9e
PKRU02 <PKRU02@PKRU02s-iMac.local>
2017-07-25 13:35:28 +0700

    Variable

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This is Comment นี่คือหมายเหตุ
+
+//Implicit
+//Implicit Variable Type var คือการประกาศตัวแปร ที่สามารถแก้ใขค่าได้
+var strName = "Doramon" //Implicit Value Doramon ==> str strName
+strName = "Nopita"
+
+//Implicit Varable Type let คือการประกาศค่าคงที่ที่ไม่สามารถแก้ใขค่าได้
+let steSurname = "ABC"
+//steSurname = "DEF" Error
