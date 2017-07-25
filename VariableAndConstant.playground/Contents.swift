94d11a9ab64d3648d4a24667510ee77938b1a866
PKRU02 <PKRU02@PKRU02s-iMac.local>
2017-07-25 13:51:51 +0700

    DataType

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -12,3 +12,27 @@ strName = "Nopita"
 //Implicit Varable Type let คือการประกาศค่าคงที่ที่ไม่สามารถแก้ใขค่าได้
 let steSurname = "ABC"
 //steSurname = "DEF" Error
+
+//Integer
+let intNumber = 123
+
+//Double
+let douNumber = 12.5242
+
+//Bool
+var boolStatus = true
+boolStatus = false
+
+//Explicit คือการกำหนด Data type ==> Varible
+let strStudent: String = "Somchai"
+
+let intNumber2: Int = 1
+
+var douNumber2: Double = 123.456
+douNumber2 = 456.321
+
+let bolStatus2: Bool = false
+
+
+
+
